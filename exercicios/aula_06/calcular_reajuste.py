c508d4641463f6b814c1b1f127fd928db770d47c
SGMorbey <sogonca@hotmail.com>
2024-12-09 12:57:27 +0000

    Adicionando arquivos novos

---
+++ exercicios/aula_06/calcular_reajuste.py
@@ -0,0 +1,16 @@
+def calcular_reajuste(salario):
+    if 0 <= salario <= 400.00:
+        percentual = 15
+    elif 400.01 <= salario <= 800.00:
+        percentual = 12
+    elif 800.01 <= salario <= 1200.00:
+        percentual = 10
+    elif 1200.01 <= salario <= 2000.00:
+        percentual = 7
+    else:
+        percentual = 4
+
+    reajuste = salario * (percentual / 100)
+    novo_salario = salario + reajuste
+
+    return novo_salario, reajuste, percentual
